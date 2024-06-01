598f11ae89218784546ebc4962411db2e43db718
prem-24 <prem24052000@gmail.com>
2024-06-01 10:11:40 +0530

    k

--- Seeders/index.js
+++ Seeders/index.js
@@ -1,9 +1,9 @@
 const UsersData = [
   {
-    content: "Feeling blessed on this beautiful morning!",
-    icon: "blessed.png",
-    authorName: "Alice Johnson",
-    imageUrl: "/uploads/pic1.jpg",
+    content: "Believe it! 🌟 #Naruto #ShinobiWay",
+    icon: "/uploads/user1.svg",
+    authorName: "rajesh",
+    imageUrl: "/uploads/pic1.png",
     author: "60ac802a96126a1f442e3d2b", // Valid ObjectId reference to a user
     createdAt: new Date(),
     updatedAt: new Date(),
@@ -11,10 +11,10 @@ const UsersData = [
     comments: [],
   },
   {
-    content: "Cooked my favorite recipe today!",
-    icon: "chef.png",
-    authorName: "Michael Anderson",
-    imageUrl: "/uploads/pic1.jpg",
+    content: `"Push yourself, because no one else is going to do it for you." 💪 #GymLife #FitnessGoals`,
+    icon: "/uploads/user1.svg",
+    authorName: "rajesh",
+    imageUrl: "/uploads/pic2.png",
     author: "60ac802a96126a1f442e3d2c", // Valid ObjectId reference to a user
     createdAt: new Date(),
     updatedAt: new Date(),
@@ -22,10 +22,87 @@ const UsersData = [
     comments: [],
   },
   {
-    content: "Enjoying the sunset at the beach!",
-    icon: "sunset.png",
-    authorName: "Sophia Lee",
-    imageUrl: "/uploads/pic1.jpg",
+    content: `"Anime: Where dreams and reality collide." 🌟 #AnimeLove #OtakuLife`,
+    icon: "/uploads/user2.svg",
+    authorName: "Sophia",
+    imageUrl: "/uploads/pic5.png",
+    author: "60ac802a96126a1f442e3d2d", // Valid ObjectId reference to a user
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    likes: [],
+    comments: [],
+  },
+  {
+    content: `"Code your dreams into reality." 💻✨ #CodingLife #DeveloperMindset`,
+    icon: "/uploads/user3.svg",
+    authorName: "sree",
+    imageUrl: "/uploads/pic4.png",
+    author: "60ac802a96126a1f442e3d2d", // Valid ObjectId reference to a user
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    likes: [],
+    comments: [],
+  },
+  {
+    content: `"Anime: Where dreams and reality collide." 🌟 #AnimeLove #OtakuLife`,
+    icon: "/uploads/user2.svg",
+    authorName: "Sophia",
+    imageUrl: "/uploads/pic5.png",
+    author: "60ac802a96126a1f442e3d2d", // Valid ObjectId reference to a user
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    likes: [],
+    comments: [],
+  },
+  {
+    content: `"Transforming coffee into code, one line at a time." ☕💻 #CodingLife #Programmer`,
+    icon: "/uploads/user3.svg",
+    authorName: "sree",
+    imageUrl: "/uploads/pic3.png",
+    author: "60ac802a96126a1f442e3d2d", // Valid ObjectId reference to a user
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    likes: [],
+    comments: [],
+  },
+  {
+    content: `"Grace and strength wrapped in gentle resolve. 🌸 #Hinata #Byakugan"`,
+    icon: "/uploads/user3.svg",
+    authorName: "hinata",
+    imageUrl: "/uploads/pic6.png",
+    author: "60ac802a96126a1f442e3d2d", // Valid ObjectId reference to a user
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    likes: [],
+    comments: [],
+  },
+  {
+    content: `"A place where someone still thinks of you is a place you can call home. #Jiraiya`,
+    icon: "/uploads/user5.svg",
+    authorName: "jiraya",
+    imageUrl: "/uploads/pic1.png",
+    author: "60ac802a96126a1f442e3d2d", // Valid ObjectId reference to a user
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    likes: [],
+    comments: [],
+  },
+  {
+    content: `"Marvel at the grandeur of the Brihadeeswarar Temple, where history and architecture unite. 🕉️ #Thanjavur #IncredibleIndia"`,
+    icon: "/uploads/user1.svg",
+    authorName: "rajesh",
+    imageUrl: "/uploads/pic9.png",
+    author: "60ac802a96126a1f442e3d2d", // Valid ObjectId reference to a user
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    likes: [],
+    comments: [],
+  },
+  {
+    content: `"Chase those six-pack dreams with dedication and sweat. 💪 #FitnessGoals #SixPackAbs"`,
+    icon: "/uploads/pic8.svg",
+    authorName: "jhon",
+    imageUrl: "/uploads/pic11.png",
     author: "60ac802a96126a1f442e3d2d", // Valid ObjectId reference to a user
     createdAt: new Date(),
     updatedAt: new Date(),
